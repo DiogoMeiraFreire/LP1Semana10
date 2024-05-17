01d43ce3f2ee094a1d0d784877bf3af0cbf16e0f
DiogoMeiraFreire <didi9934@gmail.com>
2024-05-17 23:22:36 +0100

    Changed GuessTheNumber to have the MVC approach

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 
 namespace GuessTheNumber
 {
@@ -6,44 +6,13 @@ namespace GuessTheNumber
     {
         private static void Main()
         {
-            // Generate a random number
-            Random random = new Random();
+            Model model = new Model();
 
-            // Generate a number between 1 and 100
-            int targetNumber = random.Next(1, 101);
+            Controller controller = new Controller(model);  
 
-            int guess;
-            int attempts = 0;
-            bool guessedCorrectly = false;
+            IView view = new View(controller, model);
 
-            Console.WriteLine("Welcome to Guess the Number!");
-            Console.WriteLine("I have chosen a number between 1 and 100.");
-
-            // Game loop
-            while (!guessedCorrectly)
-            {
-                Console.Write("Take a guess: ");
-                guess = Convert.ToInt32(Console.ReadLine());
-                attempts++;
-
-                if (guess == targetNumber)
-                {
-                    Console.WriteLine(
-                        "Congratulations! You guessed the number correctly!");
-                    Console.WriteLine("Number of attempts: " + attempts);
-                    guessedCorrectly = true;
-                }
-                else if (guess < targetNumber)
-                {
-                    Console.WriteLine("Too low! Try again.");
-                }
-                else
-                {
-                    Console.WriteLine("Too high! Try again.");
-                }
-            }
-
-            Console.WriteLine("Thank you for playing Guess the Number!");
+            controller.Start(view);
         }
     }
 }
